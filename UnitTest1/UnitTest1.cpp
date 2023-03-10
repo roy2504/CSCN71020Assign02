7ab768ac384a8873ff9eaeabe822c94137da9de4
roy2504 <122998913+roy2504@users.noreply.github.com>
2023-03-10 15:30:53 -0500

    Added unit test for getArea

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -37,68 +37,68 @@ namespace UnitTest1
 
 		}
 
-		TEST_METHOD(setLength_Functionality_1)
-		{
-			//This is testing the setLength function to allow only integers from 1 - 99, inclusive, for length
+		//TEST_METHOD(setLength_Functionality_1)
+		//{
+		//	//This is testing the setLength function to allow only integers from 1 - 99, inclusive, for length
 
-			//using input = 15
-			int input1 = 15;
-			int length1 = 0;
-			setLength(input1, &length1);
-			Assert::AreEqual(input1, length1);
-		}
-		
-		TEST_METHOD(setLength_Functionality_2)
-		{
-			//This is testing the setLength function to allow only integers from 1 - 99, inclusive, for length
-			//using input = 56
+		//	//using input = 15
+		//	int input1 = 15;
+		//	int length1 = 0;
+		//	setLength(input1, &length1);
+		//	Assert::AreEqual(input1, length1);
+		//}
+		//
+		//TEST_METHOD(setLength_Functionality_2)
+		//{
+		//	//This is testing the setLength function to allow only integers from 1 - 99, inclusive, for length
+		//	//using input = 56
 
-			int input2 = 56;
-			int length2 = 0;
-			setLength(input2, &length2);
-			Assert::AreEqual(input2, length2);
-		}
+		//	int input2 = 56;
+		//	int length2 = 0;
+		//	setLength(input2, &length2);
+		//	Assert::AreEqual(input2, length2);
+		//}
 
-		TEST_METHOD(setLength_Functionality_3)
-		{
-			//This is testing the setLength function to allow only integers from 1 - 99, inclusive, for length
-			//using input = 25
+		//TEST_METHOD(setLength_Functionality_3)
+		//{
+		//	//This is testing the setLength function to allow only integers from 1 - 99, inclusive, for length
+		//	//using input = 25
 
-			int input3 = 25;
-			int length3 = 0;
-			setLength(input3, &length3);
-			Assert::AreEqual(input3, length3);
-		}
+		//	int input3 = 25;
+		//	int length3 = 0;
+		//	setLength(input3, &length3);
+		//	Assert::AreEqual(input3, length3);
+		//}
 
-		TEST_METHOD(setWitdh_Functionality_1)
-		{
-			//This is testing the setWidth function to allow only integers from 1 to 99, inclusive, for width
+		//TEST_METHOD(setWitdh_Functionality_1)
+		//{
+		//	//This is testing the setWidth function to allow only integers from 1 to 99, inclusive, for width
 
-			int input1 = 75;
-			int width1 = 0;
-			setWidth(input1, &width1);
-			Assert::AreEqual(input1, width1);
+		//	int input1 = 75;
+		//	int width1 = 0;
+		//	setWidth(input1, &width1);
+		//	Assert::AreEqual(input1, width1);
 
-		}
+		//}
 
-		TEST_METHOD(setWitdh_Functionality_2)
-		{
-			//This is testing the setWidth function to allow only integers from 1 to 99, inclusive, for width
+		//TEST_METHOD(setWitdh_Functionality_2)
+		//{
+		//	//This is testing the setWidth function to allow only integers from 1 to 99, inclusive, for width
 
-			int input2 = 35;
-			int width2 = 0;
-			setWidth(input2, &width2);
-			Assert::AreEqual(input2, width2);
+		//	int input2 = 35;
+		//	int width2 = 0;
+		//	setWidth(input2, &width2);
+		//	Assert::AreEqual(input2, width2);
 
-		}
-		TEST_METHOD(setWitdh_Functionality_3)
-		{
-			//This is testing the setWidth function to allow only integers from 1 to 99, inclusive, for width
+		//}
+		//TEST_METHOD(setWitdh_Functionality_3)
+		//{
+		//	//This is testing the setWidth function to allow only integers from 1 to 99, inclusive, for width
 
-			int input3 = 23;
-			int width3 = 0;
-			setWidth(input3, &width3);
-			Assert::AreEqual(input3, width3);
+		//	int input3 = 23;
+		//	int width3 = 0;
+		//	setWidth(input3, &width3);
+		//	Assert::AreEqual(input3, width3);
 
 		}
 		
